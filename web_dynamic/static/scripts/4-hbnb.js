bb2b09e707f3feb20e45009043a4bc6ae5c9f607
cmlesquivel <1210@holbertonschool.com>
2020-05-17 22:43:28 +0000

    Filter places by Amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+    let my_id = [];
+
+    $("input[type=checkbox]").click(function() {
+	let my_list_name =[];
+	my_id =[]
+	
+	$('input[type=checkbox]:checked').each(function() {
+	    my_list_name.push($(this).attr('data-name'));
+	    my_id.push($(this).attr('data-id'));
+	});
+	if (my_list_name.length === 0) {
+        $('.amenities h4').html('&nbsp;');
+      } else {
+          $('.amenities h4').text(my_list_name.join(', '));
+      }
+	console.log(my_id);
+    });
+
+
+    $('.filters button').click(function(event) {
+	event.preventDefault();
+	
+	$('.places').text('');
+	
+	let obj = {}
+	obj.amenities=my_id;
+	list_places(JSON.stringify(obj));
+	
+    });
+   
+
+
+
+    $.ajax({
+	url : 'http://0.0.0.0:5001/api/v1/status/',
+	type : 'GET',
+	dataType : 'json',
+	success : function(json) {
+            $('#api_status').addClass('available');
+	},
+	
+	error : function(xhr, status) {
+            console.log('error '+ xhr);
+	},   
+    });
+
+    list_places();
+    
+    
+});
+
+
+function list_places(amenities='{}'){
+    $.ajax({
+	type:"POST",
+	url:"http://0.0.0.0:5001/api/v1/places_search",
+	dataType: 'json',
+	data: amenities,
+	contentType: "application/json; charset=utf-8",
+	success:function(places){
+	    for(let i = 0; i < places.length; i++) {
+		$('.places').append(`
+				    <article>
+				    <div class="title_box">
+				    
+				    <h2> ${places[i].name}</h2>
+				    
+				    <div class="price_by_night"> ${places[i].price_by_night} </div>
+				    
+				    </div>
+				    
+				    <div class="information">
+				    
+				    <div class="max_guest">${ places[i].max_guest}  
+				    ${ places[i].max_guest > 1 ?'Guests':'Guest' } </div>
+				    
+				    
+				    <div class="number_rooms">${places[i].number_rooms}
+				    ${ places[i].number_rooms > 1 ?'Bedrooms':'Bedroom' }  </div>
+				    
+				    <div class="number_bathrooms">${ places[i].number_bathrooms }
+				    ${ places[i].number_bathrooms > 1 ?'Bathrooms':'Bathroom' }  </div>
+				    
+				    </div>
+				    <div class="user"> 
+				    </div>
+				    
+				    <div class="description">
+				    ${places[i].description}
+				    </div>
+				    
+				    </article>
+				    `);
+	    }
+	},    
+	error : function(xhr, status) {
+	    console.log('error '+ status);
+	}    
+    });
+};
